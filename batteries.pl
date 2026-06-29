6e12132e013f88a7b1d77909b98aa62d90d1fa90
Fatima Niazi <fatimaniazi09@gmail.com>
2026-06-30 02:39:39 +0500

    Delete batteries.pl

--- batteries.pl
+++
@@ -1,8 +0,0 @@
-% battery(Brand, CapacityAh, PricePKR).
-battery('Osaka 200Ah', 200, 45000).
-battery('AGS 200Ah', 200, 42000).
-battery('Phonix 200Ah', 200, 38000).
-
-% Additional popular options from your previous search
-battery('Osaka 150Ah', 150, 36000).
-battery('AGS 150Ah', 150, 33500).
